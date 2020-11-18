72f1a8ac98553c5c3a427cb27990b09bc481c650
nooisy <61417760+nooisy@users.noreply.github.com>
2020-11-18 22:50:17 +0100

    camera

---
+++ Assets/CameraOrbit.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    protected Transform _XForm_Camera;
+    protected Transform _XForm_Parent;
+
+    protected Vector3 _LocalRotation;
+    protected float _CameraDistance = 12f;
+
+    public float MouseSensitivity = 4f;
+    public float ScrollSensitivity = 2f;
+    public float OrbitDampening = 10f;
+    public float ScrollDampening = 6f;
+
+    public bool CameraDisabled = true;
+
+    void Start()
+    {
+        this._XForm_Camera = this.transform;
+        this._XForm_Parent = this.transform.parent;
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.Space)) {
+            CameraDisabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || Input.GetKeyUp(KeyCode.Space)) {
+            CameraDisabled = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (!CameraDisabled)
+        {
+            if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
+                _LocalRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+                _LocalRotation.y = Mathf.Clamp(_LocalRotation.y, 0f, 90f);
+            }
+
+            if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+            {
+                float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitivity;
+
+                ScrollAmount *= (this._CameraDistance * 0.3f);
+
+                this._CameraDistance += ScrollAmount * -1f;
+
+                this._CameraDistance = Mathf.Clamp(this._CameraDistance, 5f, 20f);
+            }
+        }
+
+        Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+        this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
+
+        if(this._XForm_Camera.localPosition.z != this._CameraDistance * -1f)
+        {
+            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+        }
+    }
+}
